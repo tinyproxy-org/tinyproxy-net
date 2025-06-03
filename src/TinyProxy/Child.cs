414cd08425f68216ba8061d71301eead04773d3d
Dacheng Gao <successgdc@gmail.com>
2025-06-03 17:16:54 +0800

    add worker thread

--- src/TinyProxy/Child.cs
+++ src/TinyProxy/Child.cs
@@ -1,11 +1,34 @@
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 
 namespace TinyProxy;
 
 public class Child
 {
+    private static void ChildThread(object? data)
+    {
+        if (data is null)
+        {
+            Console.WriteLine("Error: Thread payload is null.");
+            return;
+        }
+
+        if (data is not Socket socket)
+        {
+            Console.WriteLine("Error: Thread payload data type expects to be Socket.");
+            return;
+        }
+
+        var buf = new byte[1024];
+
+        var len = socket.Receive(buf);
+
+        Console.WriteLine($"client data: len = {len}");
+
+        socket.Close();
+        socket.Dispose();
+    }
+
     private static Socket? ListeningSocket { get; set; }
 
     public static void ListenSockets(string address = "127.0.0.1", ushort port = 9999)
@@ -23,22 +46,16 @@ public class Child
             throw new InvalidOperationException("Listening socket is not initialized. Call ListenSockets() first.");
         }
 
-        byte[] buf = new byte[1024];
-
         while (true)
         {
             try
             {
                 var socket = ListeningSocket.Accept();
-                
+
                 Console.WriteLine($"client connected: {socket.RemoteEndPoint}");
-                
-                var len = socket.Receive(buf);
-                
-                Console.WriteLine($"client data: len = {len}, content = {Encoding.UTF8.GetString(buf[0..len])}");
-                
-                socket.Close();
-                socket.Dispose();
+
+                var thread = new Thread(ChildThread);
+                thread.Start(socket);
             }
             catch (Exception ex)
             {
